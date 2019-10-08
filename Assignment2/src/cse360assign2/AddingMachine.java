55552096e8a5ba74a8decee4e25f3389498b0406
johnsanchez <johnsanchez@johns-mbp.domain>
2019-10-08 01:49:47 -0700

    Added edited adding machine

--- Assignment2/src/cse360assign2/AddingMachine.java
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -3,28 +3,33 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String receipt=""; //keeps track of history of values
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		receipt="0"; //initial total
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total; //returns total
 	}
 	
 	public void add (int value) {
-		
+		total += value;	//adds value to total
+		receipt+= " + " + value;	//adds "+ value" to the history when used add
 	}
 	
-	public void subtract (int value) {
-		
+	public void subtract (int value) {	
+		total-=value;	//subtracts value to total
+		receipt+= " - " + value;	//adds "- value" to the history when used subtract
 	}
 		
 	public String toString () {
-		return "";
+		return receipt;	//sends history of every value
 	}
 
 	public void clear() {
-	
+		receipt="";	//clears history?
+		
 	}
 }
